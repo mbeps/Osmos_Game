222ea2690e88168e1b9bc2618f79e59b5b30fd14
Maruf Bepary <58662575+mbeps@users.noreply.github.com>
2021-03-03 19:09:00 +0000

    Draw mathod decomposed - draw_map method created for drawing map - draw_enemies method for drawing list of enemies

--- interactions.py
+++ interactions.py
@@ -8,11 +8,14 @@ class Interaction:
         self.enemies = enemies
     
     def draw(self, canvas): # Can draw from multiple objects 
-    #& Dependencies: Line.draw (ON)
-        #^ Draw Lines (could be moved to separate method)
+        self.draw_map(canvas)
+        self.draw_enemies(canvas)
+
+    def draw_map(self, canvas):
         for line in self.lines: # Iterates over all the lines in the list
             line.draw(canvas) # Calls draw method from line object
-        #^ Draw balles (could be moved to separate method)
+
+    def draw_enemies(self, canvas):
         for enemy in self.enemies:
            enemy.draw(canvas)
            self.update_ball(enemy)
