37400b8ffbab727c735045dafa78d0a46f451d90
Maruf Bepary <58662575+mbeps@users.noreply.github.com>
2021-03-09 19:00:36 +0000

    Added inheritance - For now, calls initializer from super class - No extra functionality - BUG: calling it causes error therefore no object is initialized

---
+++ Entities/player.py
@@ -0,0 +1,16 @@
+from Entities.ball import Ball
+
+class Player (Ball):
+    """Creates Player object.
+        Player will have the same functionality of Ball but will have some extra.  
+        Args:
+            Ball (Ball): super-class of Player. 
+        """
+    def __init__ (self, position, velocity, radius, color):
+        super.__init__(position, velocity, radius, color) # Use the initializer from super-class
+    
+    def bounce(self, normal):
+        return self.velocity.reflect(normal) # Bounce works by reflecting the ball in the normal
+    
+    def update(self):
+        self.position.add(self.velocity) # Everytime this method is called, the velocity will be added to make it move
