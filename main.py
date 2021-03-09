60a9fc4bb9a79780f202297fea2269d8e18b24e9
Maruf Bepary <58662575+mbeps@users.noreply.github.com>
2021-03-09 19:08:01 +0000

    Added comments for better readability - Section comments

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 500
 
 #^ MAIN:
+#^ Declaring Game Environment:
 lines = [Line(Vector(0, 0), Vector(0, CANVAS_HEIGHT)), 
         Line(Vector(CANVAS_WIDTH, 0), Vector(CANVAS_WIDTH, CANVAS_HEIGHT)), 
         Line(Vector(0, 0), Vector(CANVAS_WIDTH, 0)), 
@@ -16,7 +17,7 @@ enemies = [Ball(Vector(200, 300), Vector(-1, 2), 10, "blue"),
         Ball(Vector(500, 150), Vector(3, -2), 15, "green")]
 interaction = Interaction(lines, enemies)
 
+#^ Initialing Environment:
 frame = simplegui.create_frame("Domain", CANVAS_WIDTH, CANVAS_HEIGHT)
 frame.set_draw_handler(interaction.draw)
-
 frame.start()
