14e46d7534bb1cbdfa735a9ed83c5a5da38a5a6f
Maruf Bepary <58662575+mbeps@users.noreply.github.com>
2021-04-12 16:27:24 +0100

    Added radius size - Enitites (no mass) will show their size at the center of the ball

--- Entities/ball.py
+++ Entities/ball.py
@@ -24,16 +24,20 @@ class Ball:
             Draws point which represents center of the circle for easier compuration later. It takes initial position and colour as argument. 
             Initial position is given as Vector type for computation. Vector is converted to tuple. 
             
+            The ball will show its size (radius). 
+            This is will be drawn at the center of the ball.
+            
             Args:
                 `canvas (canvas)`: where the game play takes place.
             """
         canvas.draw_circle(self.position.get_p(), self.radius, 1, self.colour, self.colour) # Draws ball as circle
         canvas.draw_point(self.position.get_p(), self.colour) # Draws point as the center of the ball
+        canvas.draw_text(str(round(self.radius)), self.position.get_p(), 14, "Yellow") # Draw ball size at the centre of the ball
         """Adding sprites by overlay. 
             This is more easily implemented as the removing circle will not break any dependencies. 
             The image will dynamically change size as the circle size changes. 
-            """
-    
+            """  
+
     def bounce(self, normal):
         """Bounces the ball when wall is hit. 
             The ball bounces on the wall by reflecting velocity componenets along normal of the wall. 
